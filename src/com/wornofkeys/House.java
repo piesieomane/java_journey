c7116f8072981f3da00194f852db62d3dcf2bf22
piesieomane <piesieyeboah@gmail.com>
2022-09-24 21:00:57 +0000

    Create a constructor in the class

--- src/com/wornofkeys/House.java
+++ src/com/wornofkeys/House.java
@@ -1,6 +1,9 @@
 package com.wornofkeys;
 
 public class House {
+    public House(String ownername) {
+        System.out.println("Hello, i am the owner:" + ownername);
+    }
     public void print() {
         System.out.println("Hello, World, I am here ");
     }
--- src/com/wornofkeys/PiesieCourse.java
+++ src/com/wornofkeys/PiesieCourse.java
@@ -2,7 +2,10 @@ package com.wornofkeys;
 
 public class PiesieCourse {
     public static void main(String [] args) {
-        House myHouse = new House();
+        House myHouse = new House("Piesie");
         myHouse.print();
+
+        House yourHouse = new House("David");
+        yourHouse.print();
     }
 }
