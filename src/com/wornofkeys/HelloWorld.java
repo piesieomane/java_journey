4da1da1e7a179531b01e4523e6d4b10e27e0af6b
piesieomane <piesieyeboah@gmail.com>
2022-09-24 14:40:14 +0000

    Initial commit

---
+++ src/com/wornofkeys/HelloWorld.java
@@ -0,0 +1,26 @@
+package com.wornofkeys;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String [] args) {
+//        System.out.println("Hello world!!");
+//        int age = 22;
+//        String name = "Piesie";
+//        boolean isProgramming = true;
+//        System.out.println(name);
+        Scanner input = new Scanner(System.in);
+        System.out.println("The number:");
+        int number = input.nextInt();
+        if(number == 50) {
+            System.out.println("The number is 50");
+        }
+        else if(number == 51) {
+            System.out.println("The number is 51");
+        }
+        else if (number == 52) {
+            System.out.println("The number is 52");
+        }
+        else System.out.println("Out of range");
+    }
+}
