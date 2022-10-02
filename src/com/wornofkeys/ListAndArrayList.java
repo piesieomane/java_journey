bc6e00cc086bb2e55b799a6fdbfe1d0225966f12
piesieomane <piesieyeboah@gmail.com>
2022-10-02 17:03:56 +0000

    Create and delete an integer from the arraylist

--- src/com/wornofkeys/ListAndArrayList.java
+++ src/com/wornofkeys/ListAndArrayList.java
@@ -11,6 +11,12 @@ public class ListAndArrayList {
         //Create an array list with indefinite size
         List<Integer> numbers = new ArrayList<>();
 
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
 
+        numbers.remove(Integer.valueOf(2));
+
+        System.out.println(numbers);
     }
 }
