7668846ee023c8e529baa425d742e113136582fb
piesieomane <piesieyeboah@gmail.com>
2022-10-02 17:00:53 +0000

    Create an indefinite array list

---
+++ src/com/wornofkeys/ListAndArrayList.java
@@ -0,0 +1,16 @@
+package com.wornofkeys;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListAndArrayList {
+    public static void main(String [] args) {
+        // Lists and ArrayLists
+
+        // int [] numbers = {1 ,2, 3};
+        //Create an array list with indefinite size
+        List<Integer> numbers = new ArrayList<>();
+
+
+    }
+}
