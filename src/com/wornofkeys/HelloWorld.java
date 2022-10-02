8cec3138771534ffc0b0c58a424bcfd2fb893c25
piesieomane <piesieyeboah@gmail.com>
2022-10-02 16:36:37 +0000

    Try catch

--- src/com/wornofkeys/HelloWorld.java
+++ src/com/wornofkeys/HelloWorld.java
@@ -9,18 +9,21 @@ public class HelloWorld {
 //        String name = "Piesie";
 //        boolean isProgramming = true;
 //        System.out.println(name);
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("The number:");
+//        int number = input.nextInt();
+//        if(number == 50) {
+//            System.out.println("The number is 50");
+//        }
+//        else if(number == 51) {
+//            System.out.println("The number is 51");
+//        }
+//        else if (number == 52) {
+//            System.out.println("The number is 52");
+//        }
+//        else System.out.println("Out of range");
+
         Scanner input = new Scanner(System.in);
-        System.out.println("The number:");
-        int number = input.nextInt();
-        if(number == 50) {
-            System.out.println("The number is 50");
-        }
-        else if(number == 51) {
-            System.out.println("The number is 51");
-        }
-        else if (number == 52) {
-            System.out.println("The number is 52");
-        }
-        else System.out.println("Out of range");
+        System.out.println(input.nextInt());
     }
 }
